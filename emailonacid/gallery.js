14aee4483716eb324e3ed416c274878898381288
Igor Kalders <igor.kalders@selligent.com>
2025-11-10 17:10:18 +0100

    Redesign gallery

--- emailonacid/gallery.js
+++ emailonacid/gallery.js
@@ -11,70 +11,39 @@ const clientsMap = config.clients;
 const allClients = clients.map(clientId =>
     clientsMap[clientId]);
 
-const groupedClients = allClients.reduce((acc, diff) => {
-    const clientName = diff.client;
-
-    if (acc[clientName]) {
-        return {...acc, [clientName]: [...acc[clientName], diff]};
-    }
-
-    return {...acc, [clientName]: [diff]};
-}, {});
-
-const sortedGroupedClients = Object.values(groupedClients).sort((a, b) =>
-    a[0].client.localeCompare(b[0].client));
+const sortedClients = allClients.sort((a, b) =>
+    a.client.localeCompare(b.client));
 
 const htmlContent = fs.readFileSync('../output/index.html', 'utf-8');
 
-const { articleHtml, articleAttr, articleLead } =
-    /(?<articleLead>\s*)<article(?<articleAttr>[^>]*)>(?<articleHtml>.*?)<\/article>/s
+const { figureHtml, figureAttr, figureLead } =
+    /(?<figureLead>\s*)<figure(?<figureAttr>[^>]*)>(?<figureHtml>.*?)<\/figure>/s
         .exec(htmlContent).groups;
 
-const clientsHtml = sortedGroupedClients.map(group => {
+const clientsHtml = sortedClients.map(({ id, client, os, category, browser }) => {
 
-    const { figureHtml, figureAttr, figureLead } =
-        /(?<figureLead>\s*)<figure(?<figureAttr>[^>]*)>(?<figureHtml>.*?)<\/figure>/s
-            .exec(articleHtml).groups;
+    const img = `${id}.png`;
 
-    const clientName = group[0].client;
+    const title = [os, category, browser]
+        .filter(x => x)
+        .join(' / ');
 
-    const clientsString = group.map(({ id, os, category, browser }) => {
+    const transformedFigureHtml = figureHtml
+        .replace(/(href|src)="[^"]*"/sg, `$1="${img}"`)
+        .replace(/alt="[^"]*"/sg, `alt="${title}"`)
+        .replace(
+            /(<(?<element>h2[^>]*)>)(.*?)(<\/(\k<element>)>)/gs,
+            `<$<element>>${client}</$<element>>`)
+        .replace(
+            /(<(?<element>figcaption[^>]*)>)(.*?)(<\/(\k<element>)>)/gs,
+            `<$<element>>${title}</$<element>>`);
 
-        const img = `${id}.png`;
-
-        const title = [os, category, browser]
-            .filter(x => x)
-            .join(' / ');
-
-        const figureWithImg = figureHtml
-            .replace(/(href|src)="[^"]*"/sg, `$1="${img}"`);
-
-        const figureWithAltImg = figureWithImg
-            .replace(/alt="[^"]*"/sg, `alt="${title}"`);
-
-        const figureWithAltImageAndCaption = figureWithAltImg
-            .replace(
-                /(<figcaption[^>]*>)(.*)(<\/figcaption>)/,
-                `$1${title}$3`);
-
-        return `${figureLead}<figure${figureAttr}>${figureWithAltImageAndCaption}</figure>`;
-    })
-    .join('');
-
-    const articleWithTitle = articleHtml.replace(
-        /(<h2[^>]*>)(.*?)(<\/h2>)/,
-        `$1${clientName}$3`);
-
-    const articleWithContent = articleWithTitle
-        .replace(/\s*<figure[^>]*>.*<\/figure>/s,
-            clientsString);
-
-    return `${articleLead}<article${articleAttr}>${articleWithContent}</article>`;
+    return `${figureLead}<figure${figureAttr}>${transformedFigureHtml}</figure>`;
 })
 .join('');
 
 const updatedHtmlContent = htmlContent.replace(
-    /\s*<article[^>]*>.*<\/article>/s,
+    /\s*<figure[^>]*>.*<\/figure>/s,
     clientsHtml);
 
 fs.writeFileSync('../output/index.html', updatedHtmlContent);
